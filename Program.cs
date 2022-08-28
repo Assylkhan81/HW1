b2869daca4995024b7a5c1abf23e011a03ecc3de
Assylkhan81 <asylkhan@list.ru>
2022-08-28 17:41:29 +0600

    По заданному номеру дня недели вывести его название

--- Program.cs
+++ Program.cs
@@ -1,13 +1,45 @@
 ﻿// По двум заданным числам проверять является ли первое квадратом второго
-    Console.WriteLine("Введите 1-ое число ");
-    int numberA = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Введите 2-ое число ");
-    int numberB = Convert.ToInt32(Console.ReadLine());
-    if (numberA == numberB * numberB)
-        {
-            Console.WriteLine("Da");
-        }
-    else 
-        {
-            Console.WriteLine("net");
-        }
+    // Console.WriteLine("Введите 1-ое число ");
+    // int numberA = Convert.ToInt32(Console.ReadLine());
+    // Console.WriteLine("Введите 2-ое число ");
+    // int numberB = Convert.ToInt32(Console.ReadLine());
+    // if (numberA == numberB * numberB)
+    //     {
+    //         Console.WriteLine("Da");
+    //     }
+    // else 
+    //     {
+    //         Console.WriteLine("net");
+    //     }
+
+// По заданному номеру дня недели вывести его название
+Console.WriteLine("Введите число от 1 до 7");
+int week = Convert.ToInt32(Console.ReadLine());
+if (week == 1)
+    {
+        Console.WriteLine("Monday");
+    }
+else if (week == 2)
+    {
+        Console.WriteLine("Tuesday");
+    }
+else if (week == 3)
+    {
+        Console.WriteLine("Wednesday");
+    }
+else if (week == 4)
+    {
+        Console.WriteLine("Thursday");
+    }
+else if (week == 5)
+    {
+        Console.WriteLine("Friday");
+    }
+else if (week == 6)
+    {
+        Console.WriteLine("Saturday");
+    }
+else if (week == 7)
+    {
+        Console.WriteLine("Sunday");
+    }
