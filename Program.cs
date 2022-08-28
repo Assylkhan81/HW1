ad8a865cad22f7bab938f11a02c176f7d67b4bcb
Assylkhan81 <asylkhan@list.ru>
2022-08-28 18:08:28 +0600

    Найти максимальное из трех чисел

--- Program.cs
+++ Program.cs
@@ -13,33 +13,58 @@
     //     }
 
 // По заданному номеру дня недели вывести его название
-Console.WriteLine("Введите число от 1 до 7");
-int week = Convert.ToInt32(Console.ReadLine());
-if (week == 1)
-    {
-        Console.WriteLine("Monday");
-    }
-else if (week == 2)
-    {
-        Console.WriteLine("Tuesday");
-    }
-else if (week == 3)
-    {
-        Console.WriteLine("Wednesday");
-    }
-else if (week == 4)
-    {
-        Console.WriteLine("Thursday");
-    }
-else if (week == 5)
-    {
-        Console.WriteLine("Friday");
-    }
-else if (week == 6)
-    {
-        Console.WriteLine("Saturday");
-    }
-else if (week == 7)
-    {
-        Console.WriteLine("Sunday");
-    }
+    // Console.WriteLine("Введите число от 1 до 7");
+    // int week = Convert.ToInt32(Console.ReadLine());
+    // if (week == 1)
+    //     {
+    //         Console.WriteLine("Monday");
+    //     }
+    // else if (week == 2)
+    //     {
+    //         Console.WriteLine("Tuesday");
+    //     }
+    // else if (week == 3)
+    //     {
+    //         Console.WriteLine("Wednesday");
+    //     }
+    // else if (week == 4)
+    //     {
+    //         Console.WriteLine("Thursday");
+    //     }
+    // else if (week == 5)
+    //     {
+    //         Console.WriteLine("Friday");
+    //     }
+    // else if (week == 6)
+    //     {
+    //         Console.WriteLine("Saturday");
+    //     }
+    // else if (week == 7)
+    //     {
+    //         Console.WriteLine("Sunday");
+    //     }
+
+// Найти максимальное из трех чисел
+    int max;
+    Console.Write("Введите 1-ое число: ");
+    int numb1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите 2-ое число: ");
+    int numb2 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите 3-ье число: ");
+    int numb3 = Convert.ToInt32(Console.ReadLine());
+    if (numb1 > numb2 && numb1 > numb3)
+        {
+            //Console.WriteLine("max = " + numb1);
+            max = numb1;
+        }
+    else if (numb2 > numb1 && numb2 > numb3)
+        {
+            //Console.WriteLine("max = " + numb2);
+            max = numb2;
+        }
+    else
+        {
+            //Console.WriteLine("max = " + numb3);
+            max = numb3;
+        }
+            Console.WriteLine(max);
