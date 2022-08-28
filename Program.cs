1c49f65c67c0758f23f16a83a179be91e5bc1626
Assylkhan81 <asylkhan@list.ru>
2022-08-28 17:12:25 +0600

    является ли первое квадратом второго

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+    Console.WriteLine("Введите 1-ое число ");
+    int numberA = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите 2-ое число ");
+    int numberB = Convert.ToInt32(Console.ReadLine());
+    if (numberA == numberB * numberB)
+        {
+            Console.WriteLine("Da");
+        }
+    else 
+        {
+            Console.WriteLine("net");
+        }
